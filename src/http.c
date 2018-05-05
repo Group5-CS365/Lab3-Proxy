f25cea14425957e49d6d73094cabe0bd8c0093e2
Zeyad Ayoub <zeyadayoub@hotmail.com>
2018-05-04 23:37:49 -0700

    last check

--- src/http.c
+++ src/http.c
@@ -6,7 +6,7 @@
 /*
   MIT License
 
-  Copyright (c) 2018 Ryan Moeller, Tyler Gearing, Alex Elkins
+  Copyright (c) 2018 Ryan Moeller, Tyler Gearing, Alex Elkins, Zeyad Ayoub
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
--- src/proxy.c
+++ src/proxy.c
@@ -6,7 +6,7 @@
 /*
   MIT License
 
-  Copyright (c) 2018 Ryan Moeller, Tyler Gearing
+  Copyright (c) 2018 Ryan Moeller, Tyler Gearing, Zeyad Ayoub
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
